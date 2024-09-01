dc9baaae6d9c87843206f6cd4e30933784624d4d
Sricharan Ramesh <charan1952001@gmail.com>
2024-09-01 16:54:05 +0530

    feat: basic landing page

---
+++ src/constants/game.ts
@@ -0,0 +1,32 @@
+import { Genre } from "@/mobiles/game";
+
+export const GENRES: Genre[] = [
+  {
+    name: "Bollywood",
+    id: "bollywood",
+  },
+  {
+    name: "Pop",
+    id: "pop",
+  },
+  {
+    name: "Rock",
+    id: "rock",
+  },
+  {
+    name: "Jazz",
+    id: "jazz",
+  },
+  {
+    name: "Hip hop",
+    id: "hip hop",
+  },
+  {
+    name: "Country",
+    id: "country",
+  },
+  {
+    name: "Rhythm and blues",
+    id: "rhythym_and_blues",
+  },
+];
